6d6218fafdfd7cf7686fddee6be7fabf144c5d51
Инсаф Садиков <sadikovinsaf99@gmail.com>
2024-01-26 14:55:10 +0300

    connecting w/ backend

--- src/store/api.tsx
+++ src/store/api.tsx
@@ -91,8 +91,8 @@ export const useStoreContext=()=>useContext(StoreContext)
 
 export const StoreContextProvider=({children}:ChildreType)=>{ 
      
-const path="http://localhost:5000/apiv"
-const msgURL="http://localhost:5000/tg"
+const path="https://prjct-i82f3ovq2-fasssni.vercel.app/apiv"
+const msgURL="https://prjct-i82f3ovq2-fasssni.vercel.app/tg"
 
 const [isAuth, setIsAuth]=useState<boolean>(false)
 const [isLoading, setIsLoading]=useState<boolean>(false)
